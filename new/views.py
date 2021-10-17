3fdde46c793264af1fd1edcaf38b478120b19c63
lenovo <brajeshkumar609@gmail.com>
2021-10-17 18:13:41 +0530

    new

---
+++ new/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+from .models import quiz
+
+def home(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        ins=quiz(name=name)
+        ins.save()
+    return render(request, "index.html")
+def next(request):
+    if request.method=='POST':
+        name=request.POST['name']
+        ins=quiz(name=name)
+        ins.save()
+    return render(request, "question1.html")
